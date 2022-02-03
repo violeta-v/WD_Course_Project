a6c62516853287224661210699f4086f1f8bc018
violeta-v <92818755+violeta-v@users.noreply.github.com>
2022-02-03 15:22:50 +0000

    Add files via upload

---
+++ site_root/js/yogaAccessories.js
@@ -0,0 +1,9 @@
+function displayImageOne(elem) {
+    var image = document.getElementById("imageOne");
+    image.src = elem.value;        
+  }
+
+  function displayImageTwo(elem) {
+    var image = document.getElementById("imageTwo");
+    image.src = elem.value;        
+  }
---
+++ site_root/js/yogaMatsFunction.js
@@ -0,0 +1,9 @@
+function displayImage(elem) {
+    var image = document.getElementById("canvas");
+    image.src = elem.value;        
+  }
+
+  function displayImageTwo(elem) {
+    var image = document.getElementById("canvastwo");
+    image.src = elem.value;        
+  }
